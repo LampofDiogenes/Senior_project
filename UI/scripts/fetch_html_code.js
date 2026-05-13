0a4fcc3c15833f6019b49b3250c6b36e52cc4cad
Wil-Bradford <wannabdiogenes@gmail.com>
2026-05-13 15:48:10 -0800

    code now correctly names the files

--- UI/scripts/fetch_html_code.js
+++ UI/scripts/fetch_html_code.js
@@ -22,20 +22,31 @@ async function fetch_html_code(URL)
     console.log("html code has been fetched");
 }
 
-// Creates the path for the file
-// INCOMPLETE!!!!
+// FUTURE BUG!! 
+// Will need to check to see if the file already exists. If it does, it needs to compare instead
 function create_file(URL, data)
 {
-    // Step 1  : if the website name starts with https : remove the first 13 characters,
-    // Step 1a : if the website name starts with http : remove the first 12 characters instead
-    // Step 2  : remove the last 4 characters at the end of the website.
-    // Step 3  : convert any '/' to '-'
-    // step 4  : set website name to the remaining characters
+    
+    let URL_name = 'naming_fail'
+    const first_letters = URL.slice(0,4)
+    console.log(first_letters)
 
+    // Step 1  : if the website name starts with https : remove the first 12 characters,
+    // Step 1a : if the website name starts with http : remove the first 11 characters instead
+    if (URL.slice(0, 4) === 'http')
+    {
 
+        // Step 2  : remove the last 4 characters at the end of the website.
+        // step 4  : set website name to the remaining characters
+        const array = URL.split("."); 
+        URL_name = array[1];
+
+        // Step 3  : convert any '/' to '-'
+        URL_name = URL_name.replaceAll('/', '-');
+    }
+    
     // Step 5  : create the file, placing it into the scrapes folder
-    const file_name = "testing1"
-    const file_path = "UI/scrapes/" + file_name
+    const file_path = "UI/scrapes/" + URL_name
     const content = data
     const format = 'UTF-8'
 
