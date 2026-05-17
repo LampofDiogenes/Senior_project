2aaa263fa56b5dbeabf74983448d1766077348aa
Wil-Bradford <wannabdiogenes@gmail.com>
2026-05-16 23:13:03 -0800

    before messing with cursor

--- UI/scripts/display_scrapes.js
+++ UI/scripts/display_scrapes.js
@@ -8,12 +8,24 @@
 // insert divs into saved scrapes via javascript
 
 
-function readfiles(directory)
+function readfiles(path)
 {
     // Source - https://stackoverflow.com/a/62602038
     // Posted by dreamLo
     // Retrieved 2026-05-15, License - CC BY-SA 4.0
-    const length = window.nodeFunctions.readdirSync(directory).length
+    const length = window.nodeFunctions.readdirSync(path).length
+    const folder = window.nodeFunctions.readdirSync(path)
     console.log(length)
+    console.log(folder)
 }
 
+function makeUI(path)
+{
+    const folder = window.nodeFunctions.readdirSync(path)
+
+    for (let i = 0; i < folder.length; i++)
+    {
+        document.getElementById()
+    }
+    
+}
--- UI/scripts/fetch_navigation.js
+++ UI/scripts/fetch_navigation.js
@@ -1,4 +1,4 @@
-fetch('navigation.html')
+fetch('inserted_html_files/navigation.html')
 .then(res => res.text())
 .then(data => {
     document.getElementById('nav_bar').innerHTML = data;
