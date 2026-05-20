a8c319570f79eb3f821060a1fd3b5031f14e25eb
Wil-Bradford <wannabdiogenes@gmail.com>
2026-05-19 23:23:03 -0800

    day completed, starting to plan remodelling the application

--- UI/scripts/display_scrapes.js
+++ UI/scripts/display_scrapes.js
@@ -1,5 +1,3 @@
-
-
 // for each file in scrapes : 
     // create an entry in saved_scrapes.html that is clickable (button)
 
@@ -7,30 +5,21 @@
 // insert a copy of each of these copied formats to be inserted into the html file
 // insert divs into saved scrapes via javascript
 
-
-function readfiles(path)
-{
-    // Source - https://stackoverflow.com/a/62602038
-    // Posted by dreamLo
-    // Retrieved 2026-05-15, License - CC BY-SA 4.0
-    const length = window.nodeFunctions.readdirSync(path).length
-    const folder = window.nodeFunctions.readdirSync(path)
-    console.log(length)
-    console.log(folder)
-}
-
-function makeUI(path, parent_div)
+function makeUI(path, table)
 {
     const folder = window.nodeFunctions.readdirSync(path)
     let file_name = 'error'
 
     for (let i = 0; i < folder.length; i++)
     {
+
         file_name = folder[i] // find the file
-        let element = document.createElement('div') // make the element have the name of the filename
-        element.textContent = file_name  // make the element's content be the filename
-        element.id = file_name
-        parent_div.appendChild(element) // add the element to the DOM
+        let element = document.createElement('td') // set the element to be a table data
+        element.textContent = i + '.) ' + file_name  // make the element's content be the filename
+        element.id = file_name // make the element have the name of the filename
+        
+        let row = document.createElement('tr') // create a row
+        row.appendChild(element) // put the element in a row
+        table.appendChild(row) // add the row to the table
     }
-    
 }
---
+++ UI/scripts/set_date.js
@@ -0,0 +1,32 @@
+
+// AT SCRAPING : 
+// find the current date (today)
+// find when the date will be x weeks / days from now
+// determine the day at which web gnome will scrape
+    // update the input scrape and saved scrape UI so that they can change the elements
+
+// should I make classes for each of the scrape targets?
+    // each class contains:
+        // content
+        // date made
+        // when to scrape again (recurring)
+
+// TO DO:
+    // needs to be able to determine the next time that web gnome will scrape AT SCRAPING
+    // change the scrapes to instead be folders, which holds:
+        // date made
+        // date when to scrape again
+        // data from most current scrape
+        // data from previous to last current scrape
+
+function when_scrape_again()
+{
+    const today = new Date()
+    const month = today.getMonth()
+    const day = today.getDay()
+
+}
+
+
+
+
