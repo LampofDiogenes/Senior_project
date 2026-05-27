cb2037135e34ac4014e4b0875141299549adab1f
Wil-Bradford <wannabdiogenes@gmail.com>
2026-05-26 17:46:29 -0800

    hobart-brothers function now tracks each of the products, and stores the data from the relevant table

--- UI/scripts/jacob_script.js
+++ UI/scripts/jacob_script.js
@@ -18,113 +18,144 @@
 
 async function hobart_brothers(
     base_URL = 'https://www.hobartbrothers.com/products/?_paged=',
-    number_of_pages = 2 // change to 11
+    number_of_pages = 9
 )
 {
-
+    number_of_pages += 1 // so it counts correctly
     for (let i=1; i < number_of_pages; i++)
     {
-        // go to the product page
         let URL = base_URL + i
-        let response = await fetch(URL);
-        console.log(response); // BUG : 'failed to load resource' - hobart_brothers
-        if (!response.ok){
-            throw new Error(`Response status: ${response.status}`);
+        let product_page = await load_page(URL)
+
+        // create a folder for hobart brothers in the scrapes folder
+        let hobart_path = './UI/scrapes/hobartbrothers'
+        if (!window.nodeFunctions.existsSync(hobart_path))
+        {
+            window.nodeFunctions.mkdirSync(hobart_path)
         }
-        let product_page = await response.text();
 
         console.log('function hobart brothers has run')
-        grab_individual_products(product_page)
-    }
-
-    
+        grab_individual_products(product_page, hobart_path)
+    } 
 }
 
 // need to find the product itself:
 // make a for loop, finding each thing having "'fwpl-result r' + x"
 // this finds the product itself
 // go to that URL 
-function grab_individual_products(product_page)
+async function grab_individual_products(product_page, hobart_path)
 {
     // since the html should return as a string,
     // split everything into words, and put them in an array
     let product_array = product_page.split("<")
+    let url_focus = 'www.hobartbrothers.com/product/product-details'
+    let found_urls = []
 
     for (let i =0; i < product_array.length; i++)
     {
+
+        let unfiltered_url = product_array[i]
+
         // if anything in the array starts with 'href', go to that address
-        if (product_array[i].includes('href="http'))
+        if (unfiltered_url.includes('href="http') && unfiltered_url.includes(url_focus))
         {
-            let unfiltered_url = product_array[i]
-
-            // console.log('unfiltered : ', unfiltered_url)
-
-
             let url_beginning = unfiltered_url.indexOf('href="') + 6 // killing href="
             let url_end = unfiltered_url.length
             let target_url = product_array[i].slice(url_beginning, url_end)
-            let new_url_end = target_url.indexOf('"')
+            
+            let new_url_end = target_url.indexOf('"') // removing everything after "
 
+            // string should now have all data between href=" ... "
+            // target url is the ... now
             target_url = target_url.slice(0, new_url_end)
+            found_urls.push(target_url)
 
-            console.log('filtered: ', target_url)
-            // console.log(" ")
-            //grab_table_from_product(target_url)
+            console.log(target_url)
+
+
+            // create a folder for the product itself
+            let product_name = target_url.replace(url_focus, "") 
+            product_name = product_name.replace('https://', "")
+            let product_path = hobart_path + product_name
+
+            if (!window.nodeFunctions.existsSync(product_path))
+            {
+                window.nodeFunctions.mkdirSync(product_path)
+            }
+
+            await grab_table_from_product(target_url, product_path)
         }
     }
-    console.log('grab_individual_products function completed')
 }
 
-// find the table with the id : ='available-products'
-// this has all the sub-products (information we want)
-// store this information, and keep it in a place so we can compare again later
+// WARNING! WILL OVERWRITE PREVIOUS SCRAPES CURRENTLY!!!
 async function grab_table_from_product(
-    URL='NEED TO ADD SOMETHING HERE',
+    URL,
+    product_path,
     target='id=available_products')
 {
         // load the page
-        let response = await fetch(URL);
-        console.log(response);
-        if (!response.ok){
-            throw new Error(`Response status: ${response.status}`);
-        }    
-        let response_page = await response.text()
-
+        let response_page = await load_page(URL)
 
         // split the page based on elements
         let array = response_page.split("<")
-        console.log(array)
-        let table_start = -1
-        let table_end = -1
+        let table_array = []
+        let table_tracker = []
 
 
         // find the first table element
         for (let i=1; i < array.length; i++)
         {
-            if (array[i].includes('table') && table_start === NaN)
+            if (array[i].startsWith('table'))
             {
-                console.log('table_start is : ', array[i])
-                table_start = array[i]
+                table_array.push(array[i])
+                table_tracker.push('table_start')
             }
-            else if (array[i].includes('/table') && table_end === NaN)
+            else if (array[i].startsWith('/table'))
             {
-                console.log('table_end is : ', array[i])
-                table_end = array[i]
-                break
+                table_array.push(array[i])
+                table_tracker.push('table_end')
             }
         }
-
+        
+        
         // remove everything but the table
-        if ((table_start !== -1) && (table_end !== -1))
+        let table = 'this page does not have a table'
+        if ( table_array != [] )
         {
-            console.log('table_start is : ', table_start)
-            console.log('table_end is : ', table_end)
-            array = array.slice(table_start, table_end)
-            // log the table (later we are going to put this in a file)
-            console.log('the table of this product is: ', array)
-        }
-        else
-        {
-            console.log('this page does not have a table')
-        }    
+            let table_start = table_array[0]
+            let table_end = table_array[1]
+
+            // take the whole page, and slice it at the place where the table starts and ends
+            let start_index = response_page.indexOf(table_start) - 6 // table back in
+            let end_index = response_page.indexOf(table_end) + 7 // adding /table back in
+
+            table = response_page.slice(start_index, end_index)
+        }  
+
+        // create a file in the product's folder
+        // WARNING : will overwrite the previous scrape currently
+        let product_content = product_path + '/table'
+        window.nodeFunctions.createFile(product_content, table, 'utf-8')
+}
+
+
+async function load_page(URL)
+{
+    // let response = await fetch(URL);
+    // console.log(response);
+    // if (!response.ok){
+    //     throw new Error(`Response status: ${response.status}`);
+    // }    
+    // let response_page = await response.text()
+    // return response_page
+
+    
+    let response = await fetch(URL);
+    console.log(response); 
+    if (!response.ok){
+        throw new Error(`Response status: ${response.status}`);
+    }
+    let response_page = await response.text();
+    return response_page
 }
